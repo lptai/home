bc91cc1447894981479f958748a6937f05cb2d9b
phuctaile <tai.le1@tyme.com>
2019-12-29 10:29:39 +0700

    remove build folder

--- public/styles-ccc2f9ffffe671cf1d18.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"6M56":function(n,o,p){},Jrdj:function(n,o,p){},TpNv:function(n,o,p){}}]);
-//# sourceMappingURL=styles-ccc2f9ffffe671cf1d18.js.map
