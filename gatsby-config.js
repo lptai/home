724ba7190a558fbb240e5ecb771b3222d21df327
phuctaile <tai.le1@tyme.com>
2019-12-29 16:00:23 +0700

    fix font loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
         google: {
-          families: ["Montserrat","Helvetica Neue", "Helvetica", "Arial", "Lucida Grande", "sans-serif"],
+          families: ["Montserrat", "Arial", "Lucida Grande", "sans-serif"],
         },
       },
     },
