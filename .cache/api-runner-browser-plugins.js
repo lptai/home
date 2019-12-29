4dfafda019ef15d6ec1e46cde69b1282b01a46c1
phuctaile <tai.le1@tyme.com>
2019-12-29 11:03:34 +0700

    remove cache folder

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-web-font-loader/gatsby-browser.js'),
-      options: {"plugins":[],"google":{"families":["Montserrat","Helvetica Neue","Helvetica","Arial","Lucida Grande","sans-serif"]}},
-    }]
